489eaf49c2850f3782856c4385e49e16ef675f46
bri <gh@ibeep.com>
2023-09-19 20:23:49 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use random_word::Lang;
+use std::net;
+
+struct HostPair {
+    name: String,
+    ip: net::IpAddr,
+}
+
+impl Default for HostPair {
+    fn default() -> Self {
+        HostPair{name: generate_hostname(2), ip: [0,0,0,0].into()}
+    }
+}
+
+fn main() {
+    let host = HostPair{ip: [192,168,12,34].into(), ..Default::default()};
+    println!("name: {}, ip: {}", host.name, host.ip);
+}
+
+fn generate_hostname(wordcount: usize) -> String {
+    assert!(wordcount >= 1);
+    let mut words: Vec<String> = Vec::with_capacity(wordcount);
+    for _ in 0..wordcount{
+        words.push(String::from(random_word::gen(Lang::En)));        
+    };
+    let mut hostname = String::from(words.pop().unwrap());
+    loop {
+        match words.pop() {
+           Some(word) => {
+                hostname = hostname + "-" + &word;
+           }
+           _ => break
+        }
+    }
+    hostname
+}
+
